1536fc18fed4a2e1ba5acbe99d16f3f99ad384ca
EMRE <1907burem@gmail.com>
2021-01-16 21:38:42 +0300

    Proje dosyası ekle.

---
+++ Methods/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class Product
+    {
+        string name = "Elma";
+        double cost = 15;
+        string description = "Amasya elması";
+    }
+}
